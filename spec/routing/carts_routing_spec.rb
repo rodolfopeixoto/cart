82aa70a38cf1a1dd9b77334e7783d74bb90523c6
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-10-28 15:06:52 -0200

    Created routes for cart

---
+++ spec/routing/carts_routing_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe CartsController, type: :routing do
+  describe "routing" do
+    it "routes to #add" do
+      expect(put: "cart/add/1").to route_to("carts#add", product_id: "1")
+    end
+    it "routes do #remove" do
+      expect(put: "cart/remove/1").to route_to("carts#remove", product_id: "1")
+    end
+  end
+end
